81eb6701553e9bf4cad6d93a5f23f27b2753de58
missvader <vanepalazon611@gmail.com>
2022-01-23 18:41:18 +0100

    ex 1 al 8

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,98 @@
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+const $name = document.querySelector('.name');
+const $lastName = document.getElementById('lastName');
+const $password = document.querySelector(".password");
+const $phone = document.querySelector('.phone');
+const $email = document.getElementById('email');
+const $address = document.getElementById('address');
+//seleccionamos tambien los inputs para poder manipularlos
+const $inputs = document.querySelectorAll('input');
 
 // Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+const $errorPassword = document.getElementById("errorPassword");
+const $errorName = document.getElementById('errorName');
+const $errorLastName = document.getElementById('errorLastName') ;
+const $errorPhone = document.getElementById('errorPhone');
+const $errorEmail = document.getElementById('errorEmail')  ;
+const $errorAddress = document.getElementById('errorAddress');
 
 // Exercise 6
-function validate() {
+const form = document.getElementById('form');
+
+
+
+function validate(event) {
+    event.preventDefault();
+
     // Validate fields entered by the user: name, phone, password, and email
+    const regName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const regPhone= /^\+?(6\d{2}|7[1-9]\d{1})\d{6}$/;
+    const regPassword = /^(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]+$/;  
+    const regAddress = /^[a-zA-Z0-9\s,.'-]{3,}$/ ;
+    //........validacion name.............
+     if(!regName.test($name.value)){
+            //mensaje de error
+        $errorName.innerHTML = "Numbers in the name are not valid";
+            //añadimos clase para poder modificar estilo cuando mostremos error
+        $name.classList.add('invalid');   
+     }else if($name.value.length < 3){
+        $errorName.innerHTML = "Please, at least 3 characters";
+        $name.classList.add('invalid');
+     } else{
+         $name.classList.add('valid');
+     }
+    
+    
+
+    //.......validacion email............
+    if(!regEmail.test($email.value)){
+            //mensaje de error + invalid class
+        $errorEmail.innerHTML = "Enter a valid email address";
+        $email.classList.add('invalid');
+        
+    }else{
+        $email.classList.add('valid');
+    }
+
+    //...........validacion address................
+    if(!regAddress.test($address.value)){
+            //mensaje error + invalid class
+        $errorAddress.innerHTML = "Invalid address";
+        $address.classList.add('invalid');
+    }else{
+        $address.classList.add('valid');
+    }
+
+    //...........validacion lastName..................
+    if(!regName.test($lastName.value)){
+            //mensaje error + invalid class
+        $errorLastName.innerHTML = "Numbers in the Lastname are not valid";
+        $lastName.classList.add('invalid');
+    }else if($lastName.value.length < 3){
+        $errorLastName.innerHTML = "Please, at least 3 characters";
+        $lastName.classList.add('invalid');
+    }else{
+        $lastName.classList.add('valid');
+    }
+
+     //.......validacion password...................
+     if(!regPassword.test($password.value)){
+            //mensaje error + invalid class
+        $errorPassword.innerHTML = "Enter a correct password";
+        $password.classList.add('invalid');
+    }else{
+        $password.classList.add('valid');
+    }
+
+    //.......validacion phone................
+    if(!regPhone.test($phone.value) ){
+            //mensaje error + invalid class
+        $errorPhone.innerHTML = "Invalid phone number!!";
+        $phone.classList.add('invalid');
+    }else{
+        $phone.classList.add('valid');
+    }
+
+   
 }
--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
 // If you have time, you can move this variable "products" to a json file and load the data in this js. It will look more professional
-var products = [
+const products = [
     {
         id: 1,
         name: 'cooking oil',
@@ -56,40 +56,95 @@ var products = [
     }
 ]
 // Array with products (objects) added directly with push(). Products in this array are repeated.
-var cartList = [];
+let cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+let cart = [];
 
-var total = 0;
+let total = 0;
 
 // Exercise 1
-function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
+// function buy(id) {
+//     // 1. Loop for to the array products to get the item to add to cart
+//     // 2. Add found product to the cartList array
+//     products.forEach(item =>{
+//         if(item.id === id){
+//             cartList.push(item);
+//         };
+//     });
+
+// }
+// buy();
+
 
 // Exercise 2
 function cleanCart() {
-
+    
+    //asignando el valor 0 a la prop length del array, automaticamiente vaciamos array
+    cartList.length = 0;
 }
 
 // Exercise 3
+let subTotal = [];
+//creamos variable que almacene los precios en array para poder iterar sobre ellos
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    //iteramos sobre array cartlist 
+for(let i=0; i< cartList.length; i++){
+    /*obtenemos la propiedad price de cada uno de los elementos array y hacemos push a    array subtotal*/
+       subTotal.push(cartList[i].price);
+       
+    }
+    // iteramos sobre array subtotal y sumamos los elementos
+     for(let i of subTotal) total +=i;
+     console.log(total);
 }
-
+calculateTotal(); 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+// function generateCart() {
+//     // Using the "cartlist" array that contains all the items in the shopping cart, 
+//     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 
+//     //iteramos sobre cada elemento array cartlist   
+//     for(let i=0; i < cartList.length; i++){
+//         /*comparamos los 2 array: la primera condicion es que el elemento no exista en array cart. En ese caso, agregamos key quantity con valor 1, y key subtotal con valor del precio. Hacemos push para añadir al array cart*/      
+//                if(!cart.includes(cartList[i])){
+//                    cartList[i].quantity = 1;
+//                    cartList[i].subtotal = cartList[i].price;
+//                    cart.push(cartList[i]);
+//         /*Elemento existe en array cart. iteramos sobre array cart y buscamos el elemento que coincida con el otro comparando alguna propiedad. En este caso, incrementamos el valor de cantidad y sumamos subtotal a subtotal existente*/            
+//                }else{
+//                    for(let j=0; j < cart.length; j++){
+//                        if(cartList[i].name === cart[j].name){
+//                            cart[j].quantity ++;
+//                            cart[j].subtotal += cart[j].price;
+//                        }
+//                    }
+//                }
+//            }
+              
+//            console.log(cart);
+// }
+// generateCart();
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
-}
 
+    //iteramos sobre array cart y comparamos si cumple condic aplicar promos    
+    for (let i = 0; i < cart.length; i++){
+        if(cart[i].id === 1 && cart[i].quantity >= 3){
+            let priceDiscount1 = 10;
+            cart[i].subtotalWithDiscount = priceDiscount1 * cart[i].quantity;
+        }else{
+            if(cart[i].id === 3 && cart[i].quantity >= 10){
+                let priceDiscount2 = (cart[i].price / 3) * 2;
+                cart[i].subtotalWithDiscount = priceDiscount2 * cart[i].quantity;
+            }
+        }
+    }
+   // console.log(cart);
+}
+//applyPromotionsCart();
 
 // ** Nivell II **
 
@@ -98,15 +153,62 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    products.forEach(item =>{
+        if(item.id === id){
+           if(!cart.includes(item)){
+               item.quantity = 1;
+               item.subtotal = item.price;
+               cart.push(item);
+           }else{
+               for(let i = 0; i < cart.length; i++){
+                   if(item.id === cart[i].id){
+                       cart[i].quantity++;
+                       cart[i].subtotal += cart[i].price
+                   }
+               }
+           }
+        };
+    });
+    applyPromotionsCart();
+    
 }
+console.log(cart);
+addToCart(1);
+addToCart(1);
+addToCart(1);
+addToCart(1);
+addToCart(3);
+addToCart(5);
+addToCart(5);
+
+
+
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+   for(let i= 0; i<cart.length; i++){
+       if(id === cart[i].id){
+           if(cart[i].quantity > 1){
+               cart[i].quantity -= 1;
+           }else if(cart[i].quantity == 1){
+               cart.splice(i,1);
+           }
+       }
+   }
 }
-
+removeFromCart();
 // Exercise 9
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    let $list = document.querySelector('.list');
+    const fragment = document.createDocumentFragment();
+
+    for(i=0; i<cart.length; i++){
+        const li = document.createElement("li"); 
+        li.textContent = cart[i].textContent;
+        const referenceNode = fragment.firstChild;
+        fragment.insertBefore(li, referenceNode);
+    }
+   $list.appendChild(fragment);
 }
+printCart();
